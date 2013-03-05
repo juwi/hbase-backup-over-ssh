8549e3bed764152ea81831ee6c4cbb8959614cb2
juwi <juwi@mk-lab-02.eit.hs-mittweida.de>
2013-03-05 16:55:54 +0100

    Added hbase script, removed rsync script

--- hbaseexport.sh
+++ hbaseexport.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# rsync between two different servers - optimized for multiple cron jobs
-# rsync.sh
-# Copyright (C) 2008-2010  Brett Alton <brett.jr.alton@gmail.com>
+# Do an hbase backup to a remote server - optimized for multiple cron jobs
+# hbaseexport.sh
+# Copyright (C) 2013  Julian Wissmann <julianwissmann@gmail.com>
+# Based on the excellent rsync-ssh backup script by Brett Alton <brett.jr.alton@gmail.com> 
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -28,7 +29,7 @@
 #		* cron
 #		* logs
 
-# USAGE: ./rsync.sh ['uninstall' | 'install' | 'run'] ['push' | 'pull'] [local_dir] [remote_user] [remote_host] [remote_dir] [remote_ssh_port]
+# USAGE: ./hbaseexport.sh ['uninstall' | 'install' | 'run'] ['push' | 'pull'] [local_dir] [remote_user] [remote_host] [remote_dir] [remote_ssh_port] [table_name]
 
 
 # === FUNCTIONS ===
@@ -46,15 +47,15 @@ function print_warn()
 function force_exit()
 {
 	echo " !! $2, exiting..."
-	echo " !! USAGE: $0 ['uninstall' | 'install' | 'run'] ['push' | 'pull'] [local_dir] [remote_user] [remote_host] [remote_dir] [remote_ssh_port]"
-	cleanup # cleanup rsync log files
+	echo " !! USAGE: $0 ['uninstall' | 'install' | 'run'] ['push' | 'pull'] [hbase_dir] [remote_user] [remote_host] [remote_dir] [remote_ssh_port] [table_name]"
+	cleanup # cleanup hbase log files
 	exit $1
 }
 
 function safe_exit()
 {
 	echo " -- Safely exiting..."
-	cleanup # cleanup rsync log files
+	cleanup # cleanup hbase log files
 	exit 0
 }
 
@@ -88,7 +89,7 @@ function create_key()
 		uninstall # no point having the program installed with no encryption key
 		force_exit 1 'Could not create encryption key'
 	fi
-	
+
 	# upload key
 	print_info 'Sending encryption key'
 	ssh-copy-id -i $LOCAL_KEY_FILE "-p $REMOTE_SSH_PORT $REMOTE_USER@$REMOTE_HOST"
@@ -107,10 +108,10 @@ function uninstall()
 	#		print_warn "Could not remove $LOCAL_BIN_FILE"
 	#	fi
 	#fi
-	
+
 	if [ -f $LOCAL_CRON_FILE ]; then
 		print_info 'Uninstalling cron file'
-		
+
 		crontab -u $LOCAL_USER -r # deletes user entire crontab
 		if [ $? -ne 0 ]; then
 			print_warn "Could not uninstall $LOCAL_USER's crontab"
@@ -137,7 +138,7 @@ function cleanup()
 	# gunzip the logfile
 	if [ -f $LOCAL_LOG_FILE ]; then
 		print_info 'Compressing log file'
-		
+
 		gzip -c $LOCAL_LOG_FILE > $LOCAL_LOG_FILE.gz
 		if [ $? -ne 0 ]; then
 			print_warn "Could not compress $LOCAL_LOG_FILE"
@@ -160,30 +161,32 @@ function cleanup()
 # %m     month (01..12)
 # %d     day of month (e.g, 01)
 # %s     seconds since 1970-01-01 00:00:00 UTC
-THEDATE=`date '+%Y%m%d-%s'` # 20071010-1192044000
+THEDATESEC=`date '+%Y%m%d-%s'` # 20071010-1192044000
 ENCRYPTION_STRENGTH=4096 # bits in length for rsa key (1024,2048,4096,8172,10240,20480,etc)
 
 # parameters
 if [ "$1" == "install" ] || [ "$1" == "run" ]; then
 	ACTION=$1 # install? uninstall? run?
 	METHOD=$2 # push? pull?
-	LOCAL_DIR=$3
+	HBASE_PATH=$3
 	REMOTE_USER=$4 # ie: brett, root
 	REMOTE_HOST=$5 # ie: 192.168.1.2, example.com, sub.domain.example.com
 	REMOTE_DIR=$6
 	REMOTE_SSH_PORT=$7 # default is 22
+	TABLE=$8
 else # uninstall or unknown
 	ACTION=$1
 	REMOTE_USER=$2
 	REMOTE_HOST=$3
 fi
 
+
 # environment
 LOCAL_USER=$USER # ie: brett, root
 LOCAL_HOME=$HOME # ie: /home/brett, /root
 
 # executable
-LOCAL_BIN_NAME='rsync.sh'
+LOCAL_BIN_NAME='hbaseexport.sh'
 LOCAL_BIN_PATH=$LOCAL_HOME/bin
 LOCAL_BIN_FILE=$LOCAL_BIN_PATH/$LOCAL_BIN_NAME # ie: /home/brett/bin/rsync.sh
 
@@ -191,18 +194,18 @@ LOCAL_BIN_FILE=$LOCAL_BIN_PATH/$LOCAL_BIN_NAME # ie: /home/brett/bin/rsync.sh
 KEY="$REMOTE_HOST-$REMOTE_USER" # 192.168.1.2-brett, example.com-root
 
 # cron
-LOCAL_CRON_NAME="rsync-$KEY.cron"
+LOCAL_CRON_NAME="hbase-$KEY.cron"
 LOCAL_CRON_PATH=$LOCAL_HOME/cron
 LOCAL_CRON_FILE=$LOCAL_CRON_PATH/$LOCAL_CRON_NAME # ie: /home/brett/cron/rsync-ssh-example.com-root.cron
 LOCAL_CRON_TIME='0 2 * * *' # 2am # minute, hour, day of month, month, day of week
 
 # rsa/dsa key
-LOCAL_KEY_NAME="rsync-$KEY" # using local username to avoid collisions
+LOCAL_KEY_NAME="hbase-$KEY" # using local username to avoid collisions
 LOCAL_KEY_PATH=$LOCAL_HOME/.ssh
 LOCAL_KEY_FILE=$LOCAL_KEY_PATH/$LOCAL_KEY_NAME # ie: /home/brett/.ssh/altonlabs-rsync-ssh
 
 # log
-LOCAL_LOG_NAME="rsync-$KEY-$THEDATE.log"
+LOCAL_LOG_NAME="hbase-$KEY-$THEDATE.log"
 LOCAL_LOG_PATH=$LOCAL_HOME/logs
 LOCAL_LOG_FILE=$LOCAL_LOG_PATH/$LOCAL_LOG_NAME # ie: /home/brett/logs/rsync-20071010-1192044000.log
 LOCAL_LOG_GZ_FILE=$LOCAL_LOG_FILE.gz # ie: /home/brett/logs/rsync-20071010-1192044000.log.gz
@@ -212,27 +215,33 @@ LOCAL_LOG_GZ_FILE=$LOCAL_LOG_FILE.gz # ie: /home/brett/logs/rsync-20071010-11920
 # --- UPLOADING (push) / DOWNLOADING (pull) ---
 if [ "$ACTION" == "run" ]; then
 
-	# need 7 parameters to continue
-	if [ $# -ne 7 ]; then
+	# need 8 parameters to continue
+	if [ $# -ne 8 ]; then
 		force_exit 1 "Improper number of parameters ($#)"
 	fi
 
 	check_dir $LOCAL_LOG_PATH
 
-	# run rsync
-	# -a, --archive               archive mode; equals -rlptgoD (no -H,-A,-X)
-	# -v, --verbose               increase verbosity
-	# -z, --compress              compress file data during the transfer
-	# -e, --rsh=COMMAND           specify the remote shell to use
-	#     --delete                delete extraneous files from dest dirs
-	#     --log-file=FILE         log what we're doing to the specified FILE
-	
-	# TODO: apperently Red Hat/Fedora/CentOS doesn't have the --log-file option in rsync, so I must add Debian/Ubuntu vs CentOS detection
-	
+	# run hadoop
+	# hadoop jar $PATH_TO HBASE.jar export $TABLE $LOCAL_FILE
+	# hadoop fs -cat $LOCAL_FILE | ssh $REMOTE_HOST "dd of=$TARGET_FILE"
+
 	if [ "$METHOD" == "push" ]; then # local to remote
-		rsync -avz --rsh="ssh -l $REMOTE_USER -p $REMOTE_SSH_PORT -i $LOCAL_KEY_FILE" --delete $LOCAL_DIR $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR # --log-file=$LOCAL_LOG_FILE
+		if [[ `ssh -p $REMOTE_SSH_PORT -i $LOCAL_KEY_FILE $REMOTE_USER@$REMOTE_HOST "test -d $REMOTE_DIR"` ]]; then
+			now=`date +"%m-%d-%Y"`
+			hadoop jar $HBASE_PATH/hbase.jar export $TABLE /export/$TABLE.backup # to require timestamp in future version
+			hadoop fs -cat /export/$TABLE.backup | ssh -p $REMOTE_SSH_PORT -i $LOCAL_KEY_FILE $REMOTE_USER@$REMOTE_HOST "dd of=$REMOTE_DIR/$TABLE.backup_$now"
+			hadoop fs -rm /export/$TABLE.backup 
+		else
+			now=`date +"%m-%d-%Y"`
+			hadoop jar $HBASE_PATH/hbase.jar export $TABLE /export/$TABLE.backup # no timestamp here
+			hadoop fs -cat /export/$TABLE.backup | ssh -p $REMOTE_SSH_PORT -i $LOCAL_KEY_FILE $REMOTE_USER@$REMOTE_HOST "dd of=$REMOTE_DIR/$TABLE.backup_$now"
+			hadoop fs -rm /export/$TABLE.backup 
+		fi
 	elif [ "$METHOD" == "pull" ]; then # remote to local
-		rsync -avz --rsh="ssh -l $REMOTE_USER -p $REMOTE_SSH_PORT -i $LOCAL_KEY_FILE" --delete $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR $LOCAL_DIR # --log-file=$LOCAL_LOG_FILE
+		ssh -p $REMOTE_SSH_PORT -i $LOCAL_KEY_FILE $REMOTE_USER@$REMOTE_HOST "cat $REMOTEDIR/* > $REMOTE_DIR/full_backup"
+		hadoop fs -put `ssh -p $REMOTE_SSH_PORT -i $LOCAL_KEY_FILE $REMOTE_USER@$REMOTE_HOST "cat $REMOTE_DIR/full_backup"` /import/$TABLE		
+		hadoop jar $HBSE_PATH/hbase.jar import $TABLE /import/$TABLE
 	else
 		echo " ** Incorrect method selected: $METHOD (should be 'push' or 'pull')" # print to screen
 		echo " ** Incorrect method selected: $METHOD (should be 'push' or 'pull')" > $LOCAL_LOG_FILE # echo to log file
@@ -241,8 +250,8 @@ if [ "$ACTION" == "run" ]; then
 # --- INSTALLING ---
 elif [ "$ACTION" == "install" ]; then
 
-	# needs 7 parameters to continue
-	if [ $# -ne 7 ]; then
+	# needs 8 parameters to continue
+	if [ $# -ne 8 ]; then
 		force_exit 7 "Improper number of parameters ($#)"
 	fi
 
@@ -267,7 +276,7 @@ elif [ "$ACTION" == "install" ]; then
 		uninstall # if you can't install it, remove anything left behind
 		force_exit 1 "Could not create cron file at $LOCAL_CRON_FILE"
 	fi
-	
+
 	# install cron
 	print_info 'Registering cron file'
 	crontab -u $LOCAL_USER $LOCAL_CRON_FILE
